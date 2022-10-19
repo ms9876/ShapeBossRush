de564a2692ba8b723eb1667a2b60c8eb5f8610b0
ksan09 <kansan060223@gmail.com>
2022-10-19 10:40:57 +0900

    에러 수정 마지막

--- Assets/01_Scripts/Dev/San/WorldScript/PlatformerWorldManager.cs
+++ Assets/01_Scripts/Dev/San/WorldScript/PlatformerWorldManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlatformerWorldManager : WorldForm
 {
-    float _gravity = 10f;
-    [SerializeField] float _jumpPower = 20f;
-    [SerializeField] LayerMask _layermask;
+    //float _gravity = 10f;
+    //[SerializeField] float _jumpPower = 20f;
+    //[SerializeField] LayerMask _layermask;
 
     public override void Move(Rigidbody2D rb, KeyCode left, KeyCode right, KeyCode up, KeyCode down)
     {
